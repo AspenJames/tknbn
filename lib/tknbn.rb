d34e8710a91b2cd4570fba4adfb1856c60a0643d
aspenjames <aspenjames@tqca.org>
2019-05-28 19:33:27 -0700

    working key repeat

--- lib/tknbn.rb
+++ lib/tknbn.rb
@@ -1,6 +1,55 @@
 require "tknbn/version"
+require 'pry'
+require 'curses'
 
 module Tknbn
   class Error < StandardError; end
-  # Your code goes here...
+	# Your code goes here...
+	
+	class App
+		attr_reader :win, :width, :height
+		@@is_running = true
+
+		def initialize
+			@win = Curses.init_screen
+			@width = Curses.cols
+			@height = Curses.lines
+			@win.keypad = true
+			Curses.noecho
+			Curses.cbreak
+			Curses.curs_set(0)
+			self.run
+		end
+
+		def run
+			input = nil
+			str = ''
+			while @@is_running
+				@win.clear
+				menu_string = "Welcome to TKNBN\n"
+				Curses.setpos(@height/2, @width/2 - menu_string.length/2)
+				Curses.addstr(menu_string)
+				Curses.setpos(@height/2 + 1, @width/2 - str.length/2)
+				Curses.addstr(str)
+				@win.refresh
+				input = @win.getch
+				# binding.pry
+				case input
+				when "q"
+					str = "#{input} key pressed"
+					Curses.setpos(@height/2 + 1, 0)
+					Curses.addstr(str)
+				else
+					str = "#{input} key pressed"
+					Curses.setpos(@height/2 + 1, 0)
+					Curses.addstr(str)
+					# @win.refresh
+					# @win.getch
+					# @@is_running = false
+				end
+			end
+			Curses.close_screen
+			puts input
+		end
+	end
 end
