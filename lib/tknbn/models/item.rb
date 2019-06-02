caa0a22ca328b1db78e114c77db2101760b4be59
aspenjames <aspenjames@tqca.org>
2019-06-02 13:43:47 -0700

    Item validations + instance methods

--- lib/tknbn/models/item.rb
+++ lib/tknbn/models/item.rb
@@ -1,3 +1,21 @@
 class Item < ActiveRecord::Base
 	belongs_to :project
+
+	# Validate that the stage is in the range of
+	# 1-3, inclusive. This validation is expected
+	# to change as new stages/features added
+	validates :stage, :inclusion => {:in => (1..3)}
+
+	def stage_name
+		case stage
+		when 1
+			"TODO"
+		when 2
+			"In Progress"
+		when 3
+			"Done"
+		else
+			"N/A"
+		end
+	end
 end
