ce119c374490100ef05a2211a132cd3484c64f8e
aspenjames <aspenjames@tqca.org>
2019-06-02 13:28:06 -0700

    project validations + instance methods

--- lib/tknbn/models/project.rb
+++ lib/tknbn/models/project.rb
@@ -1,3 +1,23 @@
 class Project < ActiveRecord::Base
 	has_many :items
+
+	validates :name, :uniqueness => true
+
+	def todo
+		self.items.filter do |i|
+			i.stage == 1
+		end
+	end
+
+	def in_progress
+		self.items.filter do |i|
+			i.stage == 2
+		end
+	end
+
+	def done
+		self.items.filter do |i|
+			i.stage == 3
+		end
+	end
 end
