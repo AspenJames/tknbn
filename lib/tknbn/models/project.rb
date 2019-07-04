f5280a1c19a885221428f2a312eb3f32d79d3dce
aspenjames <aspenjames@tqca.org>
2019-07-04 12:57:27 -0700

    tweak sorbet

--- lib/tknbn/models/project.rb
+++ lib/tknbn/models/project.rb
@@ -1,4 +1,4 @@
-# typed: true
+# typed: strict
 class Project < ActiveRecord::Base
 	extend T::Sig
 	T.unsafe(self).has_many :items
@@ -7,21 +7,21 @@ class Project < ActiveRecord::Base
 
 	sig {returns(T::Array[Item])}
 	def todo
-		T.unsafe(self).items.filter do |i|
+		items.filter do |i|
 			i.stage == 1
 		end
 	end
 
 	sig {returns(T::Array[Item])}
 	def in_progress
-		T.unsafe(self).items.filter do |i|
+		items.filter do |i|
 			i.stage == 2
 		end
 	end
 
 	sig {returns(T::Array[Item])}
 	def done
-		T.unsafe(self).items.filter do |i|
+		items.filter do |i|
 			i.stage == 3
 		end
 	end
