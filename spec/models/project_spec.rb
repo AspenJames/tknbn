8b851bf8169259151ed4d4dd71455e38c2b9bcd8
Aspen James <aspenjames@tqca.org>
2019-07-28 17:37:19 -0700

    project initialization specs

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,10 +3,25 @@ require_relative '../spec_helper.rb'
 
 RSpec.describe Project do
 	describe "initialization" do
+
+    let(:name) { "Test project - #{Time.now()}" }
+
 		it "can be initialized with proper data" do
-			p = Project.create(:name => "Test project")
+      p = Project.create(:name => name)
 
 			expect(p).to be_valid
 		end
+
+    it "is not valid without a name" do
+      p = Project.create
+
+      expect(p).not_to be_valid
+    end
+
+    it "prevents duplicating names" do
+      p1 = Project.create(:name => name)
+
+      expect(p1).not_to be_valid
+    end
 	end
 end
